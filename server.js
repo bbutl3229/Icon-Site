e7444c38920cc3023ffd74ca5b0e3fef8c2c0f62
bbutl3229 <bbutl3229@westada.org>
2025-10-14 09:28:06 -0600

    Re-linked my ejs files and got my nav/footer working along with aligning and adding slight screen support

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
 // View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
 // Serve static files (CSS, images, JS)
 app.use(express.static(path.join(__dirname, 'public')));
